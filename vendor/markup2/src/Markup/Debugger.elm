0f920549a926d4188a4498eb75d06019d1374c7b
James Carlson <jxxcarlson@gmail.com>
2021-10-13 00:47:44 -0400

    Remove debug.log

--- vendor/markup2/src/Markup/Debugger.elm
+++ vendor/markup2/src/Markup/Debugger.elm
@@ -3,49 +3,17 @@ module Markup.Debugger exposing (debug1, debug2, debug3, debug4)
 import Console
 
 
-debugMagenta =
-    True
-
-
-debugCyan =
-    True
-
-
-debugYellow =
-    True
-
-
-debugBlue =
-    True
-
-
 debug1 label =
-    if debugMagenta then
-        Debug.log (Console.black (Console.bgMagenta (" " ++ label ++ " ")))
-
-    else
-        identity
+    identity
 
 
 debug2 label =
-    if debugCyan then
-        Debug.log (Console.black (Console.bgCyan (" " ++ label ++ " ")))
-
-    else
-        identity
+    identity
 
 
 debug3 label =
-    if debugYellow then
-        Debug.log (Console.black (Console.bgYellow (" " ++ label ++ " ")))
-
-    else
-        identity
+    identity
 
 
 debug4 label =
-    if debugBlue then
-        Debug.log (Console.white (Console.bgBlue (" " ++ label ++ " ")))
-
-    else
-        identity
+    identity
